db86604ceb0b137c27ca03acddbe3bc794a36d08
Gerardo Calderón <estudianteudb-cep@iMac25.local>
2013-12-09 07:53:53 -0600

    Fin Clase 14

---
+++ Clase28TouchEvent/src/com/udb/modulo2/clase28/MainActivity.java
@@ -0,0 +1,23 @@
+package com.udb.modulo2.clase28;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		TouchDrawer viewDrawer = new TouchDrawer(this);
+		setContentView(viewDrawer);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
